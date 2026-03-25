e1babbaf197d08186bbd37c3482453b5e0f9aa5e
SvettyPanda <svetlana.vitu@refresh-academy.org>
2026-03-25 11:53:20 +0100

    inizializzazione  progetto

---
+++ piuzuppa-ui/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+function App() {
+  
+
+  return (
+    <>
+    <div className='bg-salvia'>
+     <div className="flex justify-center flex-col" >
+      <div className='flex flex-row justify-center'><h2>Utente</h2><input type="text" /></div>
+     <div><h2>Password</h2><input type="text" /></div>
+     </div>
+     <button>Accedi</button>
+    </div>
+
+    </>
+  )
+}
+
+export default App
