8d665a31fe2895cb9637e310a9fd6b3816e10fc8
SvettyPanda <svetlana.vitu@refresh-academy.org>
2026-03-26 15:50:22 +0100

    restyling pulsanti navbar come pulsanti di legno

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,11 +2,17 @@ export  function Navbar() {
   return (
     <nav className="w-full px-6 py-2 flex items-center justify-between border-b-12 border-b-amber-900 bg-giallo">
      
-      <button className="min-w-36 py-1 rounded-lg bg-bordeaux text-white shadow-lg hover:opacity-70 hover:scale-110 transition font-bold">
-       piùZuppa
+      <button
+        className="min-w-36 py-1 rounded-lg  bg-amber-800 border-b-6 border-x-2 border-x-amber-900 border-b-amber-950 text-white shadow-lg transition duration-150 hover:-translate-y-1 hover:opacity-70 active:translate-y-1 font-bold"
+        style={{ textShadow: "0 -2px 0 rgba(255,255,255,0.18), 0 1px 1px rgba(0,0,0,0.45)" }}
+      >
+        piùZuppa
       </button>
-      <button className="px-8 py-1 rounded-lg bg-bordeaux text-white shadow-lg hover:opacity-70 hover:scale-110 transition font-bold">
-       Log in
+      <button
+        className="px-8 py-1 rounded-lg bg-amber-800 border-b-6 border-x-2 border-x-amber-900 border-b-amber-950 text-white shadow-lg transition duration-150 hover:-translate-y-1 active:translate-y-1 font-bold"
+        style={{ textShadow: "0 -2px 0 rgba(255,255,255,0.22), 0 1px 1px rgba(0,0,0,0.5)" }}
+      >
+        Log in
       </button>
     </nav>
   )
