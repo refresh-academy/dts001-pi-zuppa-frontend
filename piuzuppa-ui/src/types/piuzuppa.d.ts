ca5bdf1d45d0e0825cb24beaca55af60a66ec799
IreneCrocutaCrocuta <irene.ruscelli@refresh-academy.org>
2026-03-26 10:07:24 +0100

    pulizia cartelle esterne al progetto

--- piuzuppa-ui/src/types/piuzuppa.d.ts
+++ piuzuppa-ui/src/types/piuzuppa.d.ts
@@ -7,3 +7,15 @@ livelloAccesso: "volontario" | "coordinatore",
 puntoDistribuzione:"saffi" | "battiferro" | "sandonato" | "savena",
 ruolo: "cucina" | "magazzino" | "accoglienza"
 }
+
+export type ospite = {
+    nome: string,
+    cognome: string,
+    residente: boolean,
+    dataDiNascita: Date,
+    numeroFamiliari: number,
+    professione: string,
+    telefono: number,
+    enteSegnalazione: string,
+    
+}
